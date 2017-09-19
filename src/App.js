327bd51213ac26bc7a2a5082d5ac7ac2fcfefe9e
Paul Soh <paulsoh@Pauls-MacBook-Pro.local>
2017-09-19 20:39:30 +0900

    Add template for VocabBuilderApp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react';
+import WordLists from './Pages/WordLists';
+import WordList from './Pages/WordList';
+import Quizes from './Pages/Quizes';
+import QuizSession from './Pages/QuizSession';
 import {
   BrowserRouter,
   Link,
   Route,
-} from 'react-router';
+} from 'react-router-dom';
 import 'semantic-ui/dist/semantic.css';
 
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
   render() {
     return (
-      <div
-        className="ui container"
-      >
-        <div
-          className="ui card"
-        >
-          <div className="ui header">
-            Whats up
+      <BrowserRouter>
+        <div>
+          <div className="ui attached stackable menu">
+            <div className="ui container">
+              <Link to="/word-list">
+                <a className="item">
+                  <i className="home icon"></i> Word Lists
+                </a>
+              </Link>
+              <Link to="/quizes">
+                <a
+                  className="item"
+                >
+                  <i className="tasks icon"></i> Quiz
+                </a>
+              </Link>
+            </div>
           </div>
+
+          <div className="ui divider hidden"></div>
+          <div className="ui hidden divider"></div>
+          <Route path="/quiz-session" component={QuizSession} />
+          <Route path="/quizes" component={Quizes} />
+          <Route path="/words-list" component={WordLists} />
+          <Route path="/word-list" component={WordList} />
         </div>
-      </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/Pages/Quizes.js
@@ -0,0 +1,117 @@
+import React from 'react';
+
+
+export default class Quizes extends React.Component {
+  componentDidMount = () => {
+    const quizId = this.props.match.params.quizId;
+    fetch(`http://getQuiz/${quizId}`)
+
+  }
+  render() {
+    return (
+      <div className="ui container">
+        <div className="ui large header">Test Yourself</div>
+        <div className="ui divider hidden"></div>
+
+        <div className="ui cards">
+          <div className="card">
+            <div className="content">
+              <div className="header">
+                TOEIC Words
+                &nbsp;
+                <div className="ui star rating">
+                  <div className="icon">
+                  </div>
+                  <div className="icon">
+                  </div>
+                  <div className="icon">
+                  </div>
+                </div>
+              </div>
+              <div className="description">
+                1423 words
+              </div>
+            </div>
+            <div className="ui bottom attached button">
+              <i className="add icon"></i>
+              Take Quiz
+            </div>
+          </div>
+          <div className="card">
+            <div className="content">
+              <div className="header">
+                TOEFL Words
+                &nbsp;
+                <div className="ui star rating">
+                  <div className="icon active">
+                  </div>
+                  <div className="icon active">
+                  </div>
+                  <div className="icon active">
+                  </div>
+                </div>
+              </div>
+              <div className="description">
+                852 words
+              </div>
+            </div>
+            <div className="ui bottom attached button">
+              <i className="add icon"></i>
+              Take Quiz
+            </div>
+          </div>
+          <div className="card">
+            <div className="content">
+              <div className="header">
+                GRE Words
+                &nbsp;
+                <div className="ui star rating">
+                  <div className="icon active">
+                  </div>
+                  <div className="icon active">
+                  </div>
+                  <div className="icon">
+                  </div>
+                </div>
+              </div>
+              <div className="description">
+                5342 words
+              </div>
+            </div>
+            <div className="ui bottom attached button">
+              <i className="add icon"></i>
+              Take Quiz
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="content">
+              <div className="header">
+                GMAT Words
+                &nbsp;
+                <div className="ui star rating">
+                  <div className="icon active">
+                  </div>
+                  <div className="icon">
+                  </div>
+                  <div className="icon">
+                  </div>
+                </div>
+              </div>
+              <div className="description">
+                342 words
+              </div>
+            </div>
+            <div className="ui bottom attached button">
+              <i className="add icon"></i>
+              Take Quiz
+            </div>
+          </div>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+
