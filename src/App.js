9f79898639ac349d78d57220d4e718d9684774a7
Paul Soh <paulsoh@Pauls-MacBook-Pro.local>
2017-09-19 20:26:32 +0900

    Add semantic-ui and react-router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Link,
+  Route,
+} from 'react-router';
+import 'semantic-ui/dist/semantic.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div
+        className="ui container"
+      >
+        <div
+          className="ui card"
+        >
+          <div className="ui header">
+            Whats up
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
